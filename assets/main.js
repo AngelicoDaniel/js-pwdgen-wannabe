d84a248354e6df3dae57cd2cc701cc71500e4870
Daniel <angelicodaniel93@gmail.com>
2022-10-18 16:11:05 +0200

    last commit

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,10 @@ console.log(name);
 let surname = prompt('write your surname')
 console.log(surname);
 
+let favcolour = prompt('write your favourie colour')
+console.log(favcolour)
+
+const number = 21;
+console.log(number)
+
+document.getElementById('pw').innerHTML = `your password is: ${name}${surname}${favcolour}${number}`
